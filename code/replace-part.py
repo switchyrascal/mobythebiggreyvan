ca628787af226cd0c898af5da91ce45715e11214
Jonty Pearson <jontyp@gmail.com>
2018-10-26 14:28:05 -0700

    Add some files.

---
+++ code/replace-part.py
@@ -0,0 +1,33 @@
+# Install by adding a button using the macro dialog
+# Use by selecting part you want to move then the part you want to replace,
+# and press the button
+import FreeCADGui
+
+class ReplacePart():
+    def __init__(self):
+        sel = FreeCADGui.Selection.getSelection()
+        if 2 != len(sel):
+            print('Move first selection to second selection position')
+            print('Select 2 objects exactly')
+        else :
+            self.replace_part(sel[1], sel[0])
+            FreeCADGui.updateGui()
+
+    def replace_part(self, src, dst):
+        print('Moving "' + src.Label + '" to "' + dst.Label + '"')
+        dst.Placement = src.Placement
+        for property, expression in src.ExpressionEngine:
+            if property.startswith('Placement'):
+                dst.setExpression(property, expression)
+        if src.ViewObject and dst.ViewObject:
+            dst.ViewObject.DiffuseColor = src.ViewObject.DiffuseColor
+            dst.ViewObject.DisplayMode = src.ViewObject.DisplayMode
+            dst.ViewObject.LineColor = src.ViewObject.LineColor
+            dst.ViewObject.PointColor = src.ViewObject.PointColor
+            dst.ViewObject.ShapeColor = src.ViewObject.ShapeColor
+            dst.ViewObject.Transparency = src.ViewObject.Transparency
+        label = src.Label
+        src.Label = label + ' old'
+        dst.Label = label
+
+ReplacePart()
---
+++ code/spinny-vid.py
@@ -0,0 +1,137 @@
+# Install by adding a button using the macro dialog
+# Currently hard-coded file and group ordering
+# Use by starting a screencast then pressing the button
+# To clean up:
+# ffmpeg -y -i screencast.webm -r 10 -vf scale=640:360 -ss 00:00:04 -t 00:00:27 screencast.gif
+# ffmpeg -y -i screencast.webm -ss 00:00:04 -t 00:00:27 screencast.mp4
+import FreeCAD
+import FreeCADGui
+from PySide import QtCore
+
+class Input:
+    def __init__(self):
+        self.wait = 50
+        self.next = 0
+        self.objs = []
+
+class StartState:
+    def process(self, input):
+        state = self
+        if input.next < input.wait:
+            input.next += 1
+        if input.next == input.wait:
+            input.next = 0
+            state = ShowState()
+        return state
+
+class ShowState:
+    def process(self, input):
+        state = self
+        if input.next < len(input.objs):
+            input.objs[input.next].Visibility = True
+            input.next += 1
+        if input.next == len(input.objs):
+            input.next = 0
+            state = WaitState()
+        return state
+
+class WaitState:
+    def process(self, input):
+        state = self
+        if input.next < input.wait:
+            input.next += 1
+        if input.next == input.wait:
+            input.next = 0
+            input.objs.reverse()
+            state = HideState()
+        return state
+
+class HideState:
+    def process(self, input):
+        state = self
+        if input.next < len(input.objs):
+            input.objs[input.next].Visibility = False
+            input.next += 1
+        if input.next == len(input.objs):
+            input.next = 0
+            state = FinishState()
+        return state
+
+class FinishState:
+    def process(self, input):
+        return None
+
+class SpinParts():
+    def __init__(self, order, doc):
+        self.input = Input()
+        self.state = StartState()
+        self.app = FreeCAD.getDocument(doc)
+        self.gui = FreeCADGui.getDocument(doc)
+        self.timer = QtCore.QTimer()
+        self.load_objects(order)
+        self.reset_view()
+        self.start_animation()
+
+    def handle_next_object(self):
+        self.state = self.state.process(self.input)
+        if None == self.state:
+            self.finish_animation()
+        else:
+            self.update_animation()
+
+    def load_objects(self, order):
+        for j in order:
+            for i in self.app.Objects:
+                o = self.gui.getObject(i.Name)
+                if None != o.Object:
+                    parent = self.get_parent(o.Object.getParentGroup())
+                    if (0 < len(i.OutList) and j == parent) or (0 == len(i.OutList) and 1 == len(i.InList) and j == parent):
+                        print(o.Object.Label, i.Name)
+                        self.input.objs.append(o)
+
+    def reset_view(self):
+        v = self.gui.activeView()
+        v.setAnimationEnabled(False)
+        for o in self.input.objs:
+            o.Visibility = True
+        v.viewRight()
+        v.fitAll()
+        for o in self.input.objs:
+            o.Visibility = False
+        v.redraw()
+        v.setAnimationEnabled(True)
+        v.startAnimating(0, 1, 0, 0.1)
+
+    def start_animation(self):
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL('timeout()'), self.handle_next_object)
+        self.timer.start(25)
+
+    def update_animation(self):
+        FreeCADGui.updateGui()
+
+    def finish_animation(self):
+        self.timer.stop()
+        self.gui.activeView().stopAnimating()
+
+    def get_parent(self, parent):
+        l = ''
+        while True:
+            if None == parent:
+                break
+            l = parent.Label
+            parent = parent.getParentGroup()
+        return l
+
+order = [
+    'Floor',
+    'Electrical',
+    'Plumbing',
+    'Garage',
+    'Bathroom',
+    'Bedroom',
+    'Galley',
+    'Living Area',
+    'Roof',
+    'Walls',
+]
+spinner = SpinParts(order, 'moby')
